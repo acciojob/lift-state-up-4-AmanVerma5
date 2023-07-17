64de57463e407a5120c28be3d499d56c5f9bada9
aman <amanv4775@gmail.com>
2023-07-17 14:39:06 +0530

    submitting code-Mon Jul 17 2023 14:39:03 GMT+0530 (India Standard Time)

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -25,8 +25,10 @@ const Parent=()=>{
     return (
         <div className="parent">
             <h1>Parent Component</h1>
-            <div className="itemName">Item Name<input type="text" id="itemName" value={itemName} onChange={(event)=>setItemName(event.target.value)}/></div>
-            <div className="itemPrice">Item Price<input type="text" id="itemPrice" value={itemPrice} onChange={(event)=>setItemPrice(event.target.value)}/></div>
+            <label htmlFor="itemName">Item Name:</label>
+            <input type="text" id='itemName' value={itemName} onChange={(e) => setItemName(e.target.value)} />
+            <label htmlFor="itemPrice">Item Price:</label>
+            <input type="number" id='itemPrice' value={itemPrice} onChange={(e) => setItemPrice(e.target.value)} />
             <button onClick={handleClick}>Add Item</button>
             <Child addItem={items} removeItem={removeItem}/>
         </div>
